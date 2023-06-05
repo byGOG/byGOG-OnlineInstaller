ac04ebe1f1ed725f75497301ac6ad985e7c6e775
GOG <Ond3rkor@gmail.com>
2023-06-05 11:17:52 +0300

    Add files via upload

--- OnlineInstaller/VLC-OnlineInstaller.cmd
+++ OnlineInstaller/VLC-OnlineInstaller.cmd
@@ -1,5 +1,8 @@
-@echo off
-title VLC Cevrimici Yukleyici
+@ECHO off
+SETLOCAL
+SET "OUTPUT=VLCsetup.exe"
+SET "NAME=VLC Media Player"
+TITLE %NAME% Online Installer
 
 set "arch=win32"
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" set "arch=win64"
@@ -10,14 +13,14 @@ for /f "tokens=2 delims=-" %%v in ('curl -s %url% ^| findstr /i "vlc-.*-%arch%.e
 set "installerUrl=https://download.videolan.org/vlc/last/%arch%/vlc-%version%-%arch%.exe"
 
 echo ---------------------------------------------------
-echo VLC indiriliyor: %version%
-curl -# -L -o "%TEMP%\VLCsetup.exe" "%installerUrl%"
+echo Downloading VLC: %version%
+curl -# -L -o "%TEMP%\%OUTPUT%" "%installerUrl%"
 
-echo VLC kuruluyor...
-start /wait "" "%TEMP%\VLCsetup.exe" /L=1055 /S
+echo Installing VLC...
+start /wait "" "%TEMP%\%OUTPUT%" /L=1055 /S
 
-echo Gecici dosyalar siliniyor...
-del /q "%TEMP%\VLCsetup.exe"
+echo Deleting temporary files...
+del /q "%TEMP%\%OUTPUT%"
 
 ECHO Installation completed successfully! by GOG [sordum.net]
 echo ---------------------------------------------------
