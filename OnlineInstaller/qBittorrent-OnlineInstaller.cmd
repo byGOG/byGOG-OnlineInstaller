d1370a96a8a8afbe7e14ec66737b73b1e36620e2
GOG <Ond3rkor@gmail.com>
2023-06-05 13:14:19 +0300

    Add files via upload

--- OnlineInstaller/qBittorrent-OnlineInstaller.cmd
+++ OnlineInstaller/qBittorrent-OnlineInstaller.cmd
@@ -1,14 +1,10 @@
 @ECHO OFF
 SETLOCAL
-SET OUTPUT=qbittorrent.exe
-SET NAME=qBittorrent
+SET "OUTPUT=qbittorrent_setup.exe"
+SET "NAME=qBittorrent"
 TITLE %NAME% Online Installer
 
-curl -s https://www.qbittorrent.org/download | findstr /C:"Download qBittorrent" > %temp%\version.txt
-FOR /F "tokens=6 delims=<> " %%G IN (%temp%\version.txt) DO SET VERSION=%%G
-set version=%version:v=%
-
-SET URL=https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-%VERSION%/qbittorrent_%VERSION%_lt20_qt6_x64_setup.exe/download
+SET URL=https://sourceforge.net/projects/qbittorrent/files/latest/download
 
 ECHO ---------------------------------------------------
 ECHO Downloading %NAME%:%VERSION%
@@ -22,7 +18,6 @@ POWERSHELL; Start-Process cmd -ArgumentList '/c mklink %PUBLIC%\Desktop\%NAME%.l
 
 ECHO Cleaning temporary files...
 DEL %TEMP%\%OUTPUT%
-DEL %TEMP%\version.txt
 
 ECHO Installation completed successfully! by GOG [sordum.net]
 ECHO ---------------------------------------------------
