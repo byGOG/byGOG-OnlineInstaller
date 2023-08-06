d84209001a3c47f5c216b46f22b44974286b835c
GOG <Ond3rkor@gmail.com>
2023-08-06 14:50:51 +0300

    Add files via upload

--- OnlineInstaller/K-LiteCodecPackMega-OnlineInstaller.cmd
+++ OnlineInstaller/K-LiteCodecPackMega-OnlineInstaller.cmd
@@ -4,22 +4,23 @@ SET "OUTPUT=K-Lite_Codec_Pack_Mega.exe"
 SET "NAME=K-Lite Codec Pack Mega"
 TITLE %NAME% Online Installer
 
-curl -s https://codecguide.com/download_k-lite_codec_pack_mega.htm | findstr /C:"<h4>Version " > %temp%\version.txt
-for /F "tokens=2" %%G in (%temp%\version.txt) do set version=%%G
+curl -s https://codecguide.com/features_mega.htm | findstr /C:"Contents of version " > %temp%\version.txt
+FOR /F "tokens=12 delims=<> " %%G IN (%temp%\version.txt) DO SET VERSION=%%G
 set version=%version:.=%
+set version=%version::=%
 
 SET "URL=https://files3.codecguide.com/K-Lite_Codec_Pack_%VERSION%_Mega.exe"
 
 ECHO ---------------------------------------------------
 ECHO Downloading %NAME%:%VERSION%
 	 CURL -# -L -o %TEMP%\%OUTPUT% %URL%
- 
-ECHO Installing %name%...
-	 START /wait %temp%\%OUTPUT% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
+	 
+ECHO Installing %NAME%...
+	 START /wait %TEMP%\%OUTPUT% /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
 	 
 ECHO Cleaning temporary files...
-	 DEL %temp%\%OUTPUT%
-	 DEL %temp%\version.txt
+	 DEL %TEMP%\%OUTPUT%
+	 DEL %TEMP%\version.txt
 	 
 ECHO Installation completed successfully! by GOG [sordum.net]
 ECHO ---------------------------------------------------
